74ec158a3ed748c3f2b5c9ec419debd8cb80f19b
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-05-18 19:20:34 +0100

    Add accelerator example

---
+++ examples/accelerator/main.rb
@@ -0,0 +1,26 @@
+require 'yaml'
+require 'csv'
+require 'rowan_bot'
+
+# Read zoom token from file
+zoom_token = File.read('secret.txt').chomp
+user_id = File.read('user_id.txt').chomp
+meeting_details = YAML.load_file('meeting.yaml')
+participants = CSV.read('participants.csv', headers: true).map(&:to_h)
+
+zoom_api = RowanBot::ZoomAPI.new(zoom_token)
+tasks = RowanBot::Tasks.new(zoom_api)
+
+# Use this to create a meeting
+meeting = tasks.create_weekly_zoom_meeting(user_id, meeting_details)
+
+# You can replace with a meeting id and comment out line 14
+# meeting_id = 83_005_867_157
+meeting_id = meeting['id']
+
+registrants = tasks.add_participants_to_meetings(meeting_id, participants)
+
+CSV.open('meeting_participants.csv', 'w') do |csv|
+  csv << registrants.first.keys
+  registrants.each { |registrant| csv << registrant.values }
+end
