5d07f93505021e9cb2ca76df2000fd47cd6ba53a
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-05-28 21:44:08 +0100

    Add slack api

---
+++ lib/rowan_bot/slack_api.rb
@@ -0,0 +1,46 @@
+require 'slack-ruby_client'
+
+module RowanBot
+  class SlackAPI
+    def initialize(token)
+      Slack.configure do |config|
+        config.token = token
+        raise 'Missing slack token!' unless config.token
+      end
+      @client = Slack::Web::Client.new
+    end
+
+    def create_peer_group_channels(names)
+      names.map do |name|
+        channel = client.conversations_create(name: name, is_private: true)
+        logger.info("Created channel #{name}")
+        { 'name' => channel.name, 'id' => channel.id }
+      end
+    end
+
+    def add_users_to_peer_group_channel(channel_id, user_ids)
+      logger.info("Adding users to #{channel_id}")
+      client.conversations_invite(channel: channel_id, users: user_ids.join(','))
+    end
+
+    def add_slack_ids_to_users(users)
+      slack_users = client.users_list.members
+      users.map do |user|
+        slack_user = slack_users.find { user.profile.email.eql?(email) }
+        raise 'Slack user #{email} is not in this workspace' if slack_user.nil?
+        
+        logger.info("Found user #{user['email']}")
+        user['slack_id'] = slack_user.id
+        user
+      end
+    end
+
+    private
+
+    attr_reader :client
+
+    def logger
+      RowanBot.logger
+    end
+  end
+end
