08215ce5755104090de4d27e3d8d64f67f839766
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-06-02 00:37:58 +0100

    Add dummy email to see update in action

--- examples/booster/sign_waivers.rb
+++ examples/booster/sign_waivers.rb
@@ -3,5 +3,5 @@ require 'rowan_bot'
 tasks = RowanBot::Tasks.new
 tasks.docusign_api = RowanBot::DocuSignAPI.new
 tasks.salesforce_api = RowanBot::SalesforceAPI.new
-tasks.sync_signed_waivers_to_salesforce(30)
+p tasks.sync_signed_waivers_to_salesforce(30)
  
