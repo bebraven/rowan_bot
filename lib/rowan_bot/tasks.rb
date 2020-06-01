d87f0a1e5181f07336b5c90ad09309583227591c
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-06-01 21:08:13 +0100

    Add sync task

--- lib/rowan_bot/tasks.rb
+++ lib/rowan_bot/tasks.rb
@@ -1,11 +1,12 @@
 module RowanBot
   # Tasks class for tasks to be done
   class Tasks
-    attr_writer :zoom_api, :salesforce_api
+    attr_writer :zoom_api, :salesforce_api, :docusign_api
     # I do not want to break api
-    def initialize(zoom_api = nil, salesforce_api = nil)
+    def initialize(zoom_api = nil, salesforce_api = nil, docusign_api = nil)
       @zoom_api = zoom_api
       @salesforce_api = salesforce_api
+      @docusign_api = docusign_api
     end
 
     def add_participants_to_meetings(meeting_id, participants)
@@ -24,6 +25,12 @@ module RowanBot
       zoom_api.create_meeting(user_id, meeting_details)
     end
 
+    def sync_signed_waivers_to_salesforce
+      logger.info('Started syncing waiver details to salesforce')
+      signed_emails = docusign_api.recently_signed_emails
+      salesforce_api.sign_participants_waivers_by_email(signed_emails)
+    end
+
     def assign_peer_groups_to_program(program_id, cohort_size = 10)
       logger.info('Started assigning peer groups for program')
       salesforce_api.assign_peer_groups_to_program(program_id, cohort_size)
@@ -31,7 +38,7 @@ module RowanBot
 
     private
 
-    attr_reader :zoom_api, :salesforce_api
+    attr_reader :zoom_api, :salesforce_api, :docusign_api
 
     def logger
       RowanBot.logger
