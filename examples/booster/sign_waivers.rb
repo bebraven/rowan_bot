f2f165cbf6642a01a960162275d88df90edca606
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-06-02 00:41:35 +0100

    Revert changes back

--- examples/booster/sign_waivers.rb
+++ examples/booster/sign_waivers.rb
@@ -3,5 +3,5 @@ require 'rowan_bot'
 tasks = RowanBot::Tasks.new
 tasks.docusign_api = RowanBot::DocuSignAPI.new
 tasks.salesforce_api = RowanBot::SalesforceAPI.new
-p tasks.sync_signed_waivers_to_salesforce(30)
- 
+emails = tasks.sync_signed_waivers_to_salesforce(30)
+p emails
