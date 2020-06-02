60903169c7da9506519a7ac17574d84892c0f992
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-06-02 18:35:50 +0100

    Add slack browser interactions

--- lib/rowan_bot/slack_api.rb
+++ lib/rowan_bot/slack_api.rb
@@ -1,13 +1,23 @@
+# frozen_string_literal: true
+
 require 'slack-ruby-client'
+require 'capybara/dsl'
 
 module RowanBot
+  # SlackAPI class for slack stuff
   class SlackAPI
-    def initialize(token)
+    include Capybara::DSL
+
+    def initialize(token, user, password, url)
+      Capybara.default_driver = :selenium_chrome_headless
       Slack.configure do |config|
         config.token = token
-        raise 'Missing slack token!' unless config.token
       end
+
       @client = Slack::Web::Client.new
+      @slack_user = user
+      @slack_password = password
+      @slack_url = url
     end
 
     def create_peer_group_channels(names)
@@ -28,16 +38,36 @@ module RowanBot
       users.map do |user|
         slack_user = slack_users.find { |slack_u| slack_u.profile.email.eql?(user['email']) }
         raise "Slack user #{user['email']} is not in this workspace" if slack_user.nil?
-        
+
         logger.info("Found user #{user['email']}")
         user['slack_id'] = slack_user.id
         user
       end
     end
 
+    def invite_users_to_slack(emails)
+      login_to_slack
+      emails = emails.join(', ')
+      logger.info("Adding users: #{emails}")
+
+      click_on('Add')
+      click_on('add many at once')
+      fill_in('bulk-invites-input', with: emails)
+      click_on('Add Invitees')
+      click_on('Send Invitations')
+    end
+
     private
 
-    attr_reader :client
+    attr_reader :client, :slack_user, :slack_password, :slack_url
+
+    def login_to_slack
+      logger.info('Logging into slack')
+      visit slack_url
+      fill_in('email', with: slack_user)
+      fill_in('password', with: slack_password)
+      click_on('signin_btn')
+    end
 
     def logger
       RowanBot.logger
--- rowan_bot.gemspec
+++ rowan_bot.gemspec
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 lib = File.expand_path('lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 require 'rowan_bot/version'
@@ -40,9 +42,11 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
 
+  spec.add_dependency 'capybara'
+  spec.add_dependency 'docusign_esign'
   spec.add_dependency 'faraday'
   spec.add_dependency 'restforce', '~> 4.2.2'
-  spec.add_dependency 'docusign_esign'
+  spec.add_dependency 'selenium-webdriver'
   spec.add_dependency 'slack-ruby-client'
 
   spec.add_development_dependency 'bundler', '~> 2.0'
