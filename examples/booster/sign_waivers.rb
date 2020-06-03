689ad0c9fe69c64e90a371187598193313bbbb45
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-06-03 14:10:47 +0100

    Move admin emails to env vars

--- examples/booster/sign_waivers.rb
+++ examples/booster/sign_waivers.rb
@@ -6,8 +6,11 @@ tasks = RowanBot::Tasks.new
 tasks.docusign_api = RowanBot::DocuSignAPI.new
 tasks.salesforce_api = RowanBot::SalesforceAPI.new
 tasks.slack_api = RowanBot::SlackAPI.new
+
+admin_emails = ENV.fetch('SLACK_ADMIN_EMAILS', '').split(',').map(&:strip)
+
 emails = tasks.sync_signed_waivers_to_salesforce(30)
 tasks.assign_slack_to_users(emails)
 tasks.assign_peer_groups_to_users(emails)
-tasks.assign_to_peer_group_channel_in_slack(emails, ['abdulmajid.hamza@bebraven.org'])
+tasks.assign_to_peer_group_channel_in_slack(emails, admin_emails)
 tasks.send_onboarding_notification(emails)
