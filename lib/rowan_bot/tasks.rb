685ffd13ace1b03be197abde7695a617ab7a7fb1
Abdulmajid Hamza <abdulmajid.hamza@gmail.com>
2020-08-21 00:35:36 +0100

    Update salesforce api to allow for other programs

--- lib/rowan_bot/tasks.rb
+++ lib/rowan_bot/tasks.rb
@@ -25,15 +25,19 @@ module RowanBot
       zoom_api.create_meeting(user_id, meeting_details)
     end
 
-    def sync_signed_waivers_to_salesforce(days = 1)
+    def sync_booster_signed_waivers_to_salesforce(days = 1)
       logger.info('Started syncing waiver details to salesforce')
       signed_emails = docusign_api.recently_signed_emails(days)
-      salesforce_api.sign_participants_waivers_by_email(signed_emails)
+      return [] if signed_emails.empty?
+
+      record_type = 'Booster_Student'
+      salesforce_api.find_participants_by_emails(signed_emails, record_type)
+      salesforce_api.sign_participants_waivers_by_email(signed_emails, record_type)
     end
 
-    def assign_peer_groups_to_users(emails)
+    def assign_peer_groups_to_booster_users(emails)
       logger.info("Started assigning peer groups to users: #{emails}")
-      salesforce_api.assign_peer_groups_to_user_emails(emails)
+      salesforce_api.assign_peer_groups_to_user_emails(emails, 'Booster_Student')
     end
 
     def assign_slack_to_users(emails)
@@ -41,29 +45,53 @@ module RowanBot
       slack_api.invite_users_to_slack(emails)
     end
 
-    def assign_zoom_links_to_users(emails)
+    def assign_zoom_links_for_fellows(program_id)
+      record_type = 'Fellow'
+      emails = salesforce_api.all_participants(program_id, record_type)
+      assign_zoom_links_to_participants(emails, record_type)
+    end
+
+    def assign_zoom_links_for_lcs(program_id)
+      record_type = 'Leadership_Coach'
+      emails = salesforce_api.all_participants(program_id, record_type)
+      assign_zoom_links_to_participants(emails, record_type)
+    end
+
+    def assign_zoom_links_to_booster_participants(emails)
+      assign_zoom_links_to_participants(emails, 'Booster_Student')
+    end
+
+    def assign_zoom_links_to_participants(emails, record_type)
       logger.info("Started assigning zoom links to users: #{emails}")
       emails.each do |email|
-        participant = salesforce_api.find_participant_by_email(email)
-        if participant.webinar_registration_1.nil? || participant.webinar_registration_2.nil?
-          logger.warn("Skipping: #{email} doesn't have registration_details")
-          next
-        end
+        participant = salesforce_api.participant_by_email(email, record_type)
         registration_details = {
           'email' => participant.email,
           'first_name' => participant.first_name,
           'last_name' => participant.last_name
         }
-        join_url1 = zoom_api.add_registrant(
-          participant.webinar_registration_1,
-          registration_details
-        )['join_url']
-        join_url2 = zoom_api.add_registrant(
-          participant.webinar_registration_2,
-          registration_details
-        )['join_url']
-        salesforce_api.update_participant_webinar_links(participant.id, join_url1, join_url2)
-        logger.info("Added zoom link to: #{email}")
+        join_url1 = nil
+        join_url2 = nil
+        unless participant.webinar_registration_1.nil? || participant.webinar_link_1.nil?
+          join_url1 = zoom_api.add_registrant(
+            participant.webinar_registration_1,
+            registration_details
+          )['join_url']
+        end
+
+        unless participant.webinar_registration_2.nil? || participant.webinar_link_2.nil?
+          join_url2 = zoom_api.add_registrant(
+            participant.webinar_registration_2,
+            registration_details
+          )['join_url']
+        end
+
+        if join_url1.nil? && join_url2.nil?
+          logger.warn("Skipping #{participant.email}")
+        else
+          salesforce_api.update_participant_webinar_links(participant.id, join_url1, join_url2)
+          logger.info("Added zoom link to: #{email}")
+        end
       end
     end
 
@@ -72,25 +100,35 @@ module RowanBot
       slack_api.send_onboarding_notification(emails.join(', '))
     end
 
-    def assign_to_peer_group_channel_in_slack(emails, admins)
+    def assign_to_channel_name_in_slack(emails, channel_name, admins)
+      logger.info("Started assigning users to channel in slack #{channel_name}")
+      admins = admins.map { |ad| { email: ad } }
+      users = emails
+              .map do |email|
+                { email: email, peer_group: channel_name }
+              end
+      add_users_to_peer_group_channels(users, admins)
+    end
+
+    def assign_to_booster_peer_group_channel_in_slack(emails, admins)
       logger.info('Started assigning users to cohort channels in slack')
       admins = admins.map { |ad| { email: ad } }
       users = emails
-              .map { |email| salesforce_api.find_participant_by_email(email) }
+              .map { |email| salesforce_api.find_participant_by_email(email, 'Booster_Student') }
               .map do |p|
                 pname = p.peer_group.split.last(5)
-                peer_group = ['run', pname[0].to_i.to_s] + pname[1..-1]
+                peer_group = ['peer', 'group', 'run', pname[0].to_i.to_s] + pname[1..-3]
                 { email: p.email, peer_group: peer_group.join('-').downcase }
               end
       add_users_to_peer_group_channels(users, admins)
     end
 
-    def assign_to_run_channels_in_slack(emails, admins)
+    def assign_to_booster_run_channels_in_slack(emails, admins)
       logger.info('Started assigning users to run channels in slack')
-      salesforce_api.find_booster_participants_by_emails(emails)
+      salesforce_api.find_participants_by_emails(emails, 'Booster_Student')
       admins = admins.map { |ad| { email: ad } }
       users = emails
-              .map { |email| salesforce_api.find_participant_by_email(email) }
+              .map { |email| salesforce_api.find_participant_by_email(email, 'Booster_Student') }
               .map do |p|
                 pname = p.peer_group.split.last(5)
                 peer_group = ['run', pname[0].to_i.to_s, 'announcements']
@@ -98,7 +136,6 @@ module RowanBot
               end
       add_users_to_peer_group_channels(users, admins)
     end
-
 
     def add_users_to_peer_group_channels(users, admins = [])
       channel_names = users.map { |entry| entry[:peer_group] }.uniq
