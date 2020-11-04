4ca13b2721bfc7d11f7162cbd4d8773d9d9eae72
rowtat <rowan@bebraven.org>
2020-11-04 10:21:09 -0500

    Add slack functionality

--- lib/rowan_bot/tasks.rb
+++ lib/rowan_bot/tasks.rb
@@ -201,6 +201,78 @@ module RowanBot
       end
     end
 
+    def sync_program_to_slack(program_id, slack_admins)
+      sync_program_for_record_type_to_slack(program_id, 'Fellow', slack_admins)
+      sleep(10)
+      sync_program_for_record_type_to_slack(program_id, 'Leadership_Coach', slack_admins)
+    end
+
+    def sync_program_for_record_type_to_slack(program_id, record_type, slack_admins)
+
+      all_participants = salesforce_api.all_participants(program_id, record_type)
+      participants = all_participants.filter { |participant| participant.status.eql?('Enrolled') || participant.status.eql?('Dropped') }
+
+      has_cohort = participants.filter { |participant| !participant.peer_group.nil? }
+      x = has_cohort.inject({}) do |acc, parts|
+        channel = cohort_channel_name(parts)
+        obj = { email: parts.email, is_enrolled: parts.status.eql?('Enrolled') }
+        if acc[channel].nil?
+          acc[channel] = [obj]
+        else
+          acc[channel] << obj
+        end
+        acc
+      end
+
+      slack_channels = slack_api.create_peer_group_channels(x.keys).first
+      sleep(10)
+      slack_channels = slack_channels.inject({}) do |acc, chan|
+        acc[chan[:name]] = chan[:id]
+        acc
+      end
+
+      current_pairing = {}
+      slack_channels.each do |channel, channel_id|
+        mems = slack_api.get_channel_members(channel_id)
+        mems.each do |mem|
+          current_pairing[mem] = { name: channel, id: channel_id }
+        end
+      end
+
+      x.each do |cohort_name, objs|
+        pts = objs.map { |obj| { email: obj[:email], is_enrolled: obj[:is_enrolled] } }
+        slack_users = slack_api.add_slack_ids_to_users(pts)
+        sleep(10)
+        slack_admin_users = slack_api.add_slack_ids_to_users(slack_admins)
+        sleep(10)
+        to_add = slack_admin_users.map { |u| u[:slack_id] }
+        slack_users.each do |sl_u|
+          sl_id = sl_u[:slack_id]
+          if !sl_u[:is_enrolled] && !current_pairing[sl_id].nil?
+            # User is dropped
+            slack_api.remove_user_from_channel(current_pairing[sl_id][:id], sl_id)
+            sleep(10)
+            next
+          end
+
+          if current_pairing[sl_id].nil?
+            to_add << sl_id
+          elsif !current_pairing[sl_id][:name].eql?(cohort_name)
+            to_add << sl_id
+            slack_api.remove_user_from_channel(current_pairing[sl_id][:id], sl_id)
+          end
+        end
+        slack_api.add_users_to_peer_group_channel(slack_channels[cohort_name], to_add)
+      end
+    end
+
+    def cohort_channel_name(participant)
+      day = participant.peer_group.split[1]
+      first_name = participant.peer_group_lc_name.scan(/\w+/)
+      last_name = participant.peer_group_lc_last_name.gsub(/\w+/)
+      ['cohort', day, *first_name, *last_name].join('-').downcase
+    end
+
     private
 
     attr_reader :zoom_api, :salesforce_api, :docusign_api, :slack_api
